ac7bafec0a6879c6953162bd0d7fa99756217ca0
preston <broberts9@gmail.com>
2017-05-10 23:09:23 -0700

    initial wireframe layout

---
+++ app.js
@@ -0,0 +1,26 @@
+// state
+var state = {
+    countries: [],
+    capitals: [],
+    currentQuestion: 1,
+    correct: 0
+}
+
+// check answer
+
+
+// start game / next question
+
+
+// play again
+
+
+// html template
+
+
+// render
+
+
+// listen
+
+
